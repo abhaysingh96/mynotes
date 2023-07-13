f9b10f33771cfe76f0df9629ae1a28e636890711
Abhay Singh <abhay.singh.met21@itbhu.ac.in>
2023-07-13 22:38:50 +0530

    step-9

--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -1,7 +1,8 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'dart:developer' as devtools show log;
+
 import 'package:mynotes/constants/routes.dart';
+import 'package:mynotes/services/auth/auth_exceptions.dart';
+import 'package:mynotes/services/auth/auth_service.dart';
 import 'package:mynotes/utilities/show_error_dialog.dart';
 
 class RegisterView extends StatefulWidget {
@@ -58,42 +59,38 @@ class _RegisterViewState extends State<RegisterView> {
               final email = _email.text;
               final password = _password.text;
               try {
-                await FirebaseAuth.instance.createUserWithEmailAndPassword(
-                    email: email, password: password);
-                final user = FirebaseAuth.instance.currentUser;
-                user?.sendEmailVerification();
+                await AuthService.firebase().createUser(
+                  email: email,
+                  password: password,
+                );
+                final user = AuthService.firebase().currentUser;
+                AuthService.firebase().sendEmailVerification();
                 if (context.mounted) {
                   Navigator.of(context).pushNamed(verifyEmailRoute);
                 }
-              } on FirebaseAuthException catch (e) {
-                if (e.code == 'weak-password') {
-                  await showErrorDialog(
-                    context,
-                    "Password is weak",
-                  );
-                } else if (e.code == 'email-already-in-use') {
-                  await showErrorDialog(
+              } on WeakPasswordAuthException {
+                await showErrorDialog(
+                  context,
+                  "Password is weak",
+                );
+              }on EmailAlreadyInUseAuthException{
+                await showErrorDialog(
                     context,
                     "E-mail is already in use",
                   );
-                } else if (e.code == 'invalid-email') {
-                  await showErrorDialog(
+              }on InvalidEmailAuthException{
+                await showErrorDialog(
                     context,
                     "E-mail is invalid",
                   );
-                } else {
-                  await showErrorDialog(
+              }on GenericException{
+                await showErrorDialog(
                     context,
-                    "Error: ${e.code}",
+                    "Failed to Register",
                   );
-                }
-              } catch (e) {
-                showErrorDialog(
-                  context,
-                  e.toString(),
-                );
               }
-            },
+    
+              },
             child: const Text('Register'),
           ),
           TextButton(
--- lib/views/verify_email_view.dart
+++ lib/views/verify_email_view.dart
@@ -1,8 +1,8 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'dart:developer' as devtools show log;
 
 import 'package:mynotes/constants/routes.dart';
+import 'package:mynotes/services/auth/auth_service.dart';
 
 class VerifyEmailView extends StatefulWidget {
   const VerifyEmailView({super.key});
@@ -24,14 +24,13 @@ class _VerifyEmailViewState extends State<VerifyEmailView> {
               "If you haven't receiverd a verification email, press the button below"),
           TextButton(
             onPressed: () async {
-              final user = FirebaseAuth.instance.currentUser;
-              await user?.sendEmailVerification();
+              await AuthService.firebase().sendEmailVerification();
             },
             child: const Text("Send Email Verification"),
           ),
           TextButton(
             onPressed: () async {
-              await FirebaseAuth.instance.signOut();
+              await AuthService.firebase().logOut();
               if (context.mounted) {
                 Navigator.of(context)
                     .pushNamedAndRemoveUntil(registerRoute, (route) => false);
